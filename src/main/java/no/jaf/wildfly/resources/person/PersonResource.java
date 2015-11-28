87421772e75f36fd899b2ac1da5a2a36d418d849
Jon André Fagerholm <jon.fagerholm@gmail.com>
2015-11-28 15:54:00 +0100

    Added a very small REST service

---
+++ src/main/java/no/jaf/wildfly/resources/person/PersonResource.java
@@ -0,0 +1,25 @@
+package no.jaf.wildfly.resources.person;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+@Path("person")
+public class PersonResource {
+
+    @Context
+    private UriInfo context;
+
+    public PersonResource() {
+    }
+
+    @GET
+    @Produces("text/html")
+    public String getHtml() {
+        return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
+    }
+
+}
