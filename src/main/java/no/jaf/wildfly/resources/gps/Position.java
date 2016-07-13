6ffc85ef68a7c94bd880d68d03dd28db6c9f6dc4
Jon André Fagerholm <jon.fagerholm@gmail.com>
2016-07-13 18:35:47 +0200

    Added position functionality

---
+++ src/main/java/no/jaf/wildfly/resources/gps/Position.java
@@ -0,0 +1,62 @@
+package no.jaf.wildfly.resources.gps;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+
+@Entity
+public class Position {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Version
+    private long version;
+
+    private long latitude;
+    private long longitude;
+    private long millis;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getMillis() {
+        return millis;
+    }
+
+    public void setMillis(long millis) {
+        this.millis = millis;
+    }
+}
---
+++ src/main/java/no/jaf/wildfly/resources/gps/PositionResource.java
@@ -0,0 +1,48 @@
+package no.jaf.wildfly.resources.gps;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.io.IOException;
+import java.util.Collection;
+
+//@Path("/report")
+@ApplicationPath("/gps")
+public class PositionResource extends Application {
+
+    @Context
+    private UriInfo context;
+
+    @PersistenceContext(unitName = "primary")
+    EntityManager em;
+
+    public PositionResource() {
+    }
+
+
+    @PUT
+    @Path("/{user}")
+    public Response uploadCoordinates(@PathParam("user") String user, @QueryParam("lat") Long latitude, @QueryParam("lon") Long longitude, @QueryParam("t") Long millis ) throws IOException {
+
+        Position position = new Position();
+        position.setLatitude(latitude);
+        position.setLongitude(longitude);
+        position.setMillis(millis);
+
+        em.persist(position);
+
+        return Response.status(200).build();
+    }
+
+    @GET
+    @Path("/positions")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Collection<Position> getPositions() {
+
+        Query query = em.createQuery("SELECT e FROM Position e");
+        return (Collection<Position>) query.getResultList();
+    }
+
+}
